85941eee7396f939b3d6b365b6169584224d4ea0
Chris Neale <chris@komododigital.co.uk>
2021-01-14 20:59:03 +0000

    Clean up

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -7,12 +7,9 @@ export const Sandbox = ({ url }) => (
         style={{ width:'100%', height:'500px', border:0, borderRadius: '4px', overflow:'hidden'}}
         allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
         sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-        ></iframe>
+    ></iframe>
 )
 
 Sandbox.propTypes = {
-  /**
-   * The kind prop is used to set the alert's background color
-   */
   url: t.string.isRequired,
 }
